206a313adaaf698b0b2786f099fb69086382ea48
2015-08-26 <agieselm@janus18.cs.trinity.edu>
2019-04-23 13:45:20 -0500

    Main menu view

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,15 +1,15 @@
-package edu.trinity.webapps.controllers
+package controllers
 
 import javax.inject._
 
-import edu.trinity.webapps.shared.SharedMessages
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 @Singleton
-class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class mainMenuController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index(SharedMessages.itWorks))
+  def mainmenu = Action { implicit request =>
+    Ok(views.html.menu())
   }
-
 }
