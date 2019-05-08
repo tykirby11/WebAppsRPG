31f3273ea981217aee62a815496e67c4756b1822
Jonathan Fisher <jfisher1@trinity.edu>
2019-05-07 20:36:07 -0500

    Started work on DBClient

---
+++ client/src/main/scala/edu/trinity/webapps/DBClient.scala
@@ -0,0 +1,16 @@
+package edu.trinity.webapps
+
+import org.scalajs.dom
+import scala.scalajs.js
+import org.querki.jquery._
+
+import scala.scalajs.js.annotation._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
+import edu.trinity.webapps.shared.DBShared._
+
+class DBClient {
+  
+}
