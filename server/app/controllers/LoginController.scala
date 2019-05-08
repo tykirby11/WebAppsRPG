f7c21e85ae88c94f27856f6e3da3d08ad1591125
jviltoft <jviltoft@trinity.edu>
2019-05-07 20:31:19 -0500

    updated CodeGen file and Login functionality (not tested)

--- server/app/controllers/LoginController.scala
+++ server/app/controllers/LoginController.scala
@@ -5,14 +5,56 @@ import javax.inject._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-
-
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
+import scala.concurrent.ExecutionContext
+import slick.jdbc.JdbcProfile
+import scala.concurrent.Future
 
 @Singleton
-class LoginController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+class LoginController @Inject() (protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(implicit ec: ExecutionContext)
+extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
 
-   def login = Action { implicit request =>
+   def login = Action.async { implicit request =>
+    val postBody = request.body.asFormUrlEncoded
+    postBody.map { args =>
+      
+        val user = args("username").head
+        val pass = args("password").head
+        val resultFuture = models.DBQueries.verifyLogin(user, pass, db)
+        resultFuture.map(result =>
+        if(result){
+        Redirect(routes.LoginController.menu).withSession("username" -> user)
+        }
+        else{
+          Redirect(routes.LoginController.loginPage)
+        })
+      
+    }.getOrElse(Future.successful(Redirect(routes.LoginController.loginPage)))
+  }
+   
+  def loginPage = Action {
     Ok(views.html.login())
+  }
+  
+  def menu = Action {
+    Ok(views.html.menu())
+  }
+  
+  def logout = Action { implicit request =>
+    Redirect(routes.LoginController.loginPage).withSession()
+  }
+  
+  def createUser = Action.async { implicit request =>
+    val postBody = request.body.asFormUrlEncoded
+    postBody.map { args =>
+      
+        val user = args("username").head
+        val pass = args("password").head
+        models.DBQueries.addLoginInfo(user, pass, db)
+        Future.successful(Redirect(routes.LoginController.loginPage))
+      
+    }.getOrElse(Future.successful(Redirect(routes.LoginController.loginPage)))
   }
 }
---
+++ server/app/models/DBQueries.scala
@@ -0,0 +1,70 @@
+package models
+
+import scala.concurrent.Future
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.ExecutionContext
+import Tables._
+
+object DBQueries {
+  def verifyLogin(username: String, password: String, db: Database)(implicit ec: ExecutionContext): Future[Boolean] = {
+    db.run {
+      (for (u <- User; if u.password === password && u.username === username) yield {
+        u.id
+      }).exists.result
+    }
+  }
+  
+  def addLoginInfo(username: String, password: String, db: Database)(implicit ec: ExecutionContext): Future[Int] = {
+    db.run {
+      User += UserRow(0, username, password) //0 is ignored, auto increment
+    }
+  }
+  
+  def fetchEnemies(db: Database)(implicit ec: ExecutionContext): Future[Seq[EnemiesRow]] = {
+    db.run {
+      (for (e <- Enemies) yield {
+        e
+      }).result
+    }
+  }
+  
+  def fetchBosses(db: Database)(implicit ec: ExecutionContext): Future[Seq[BossesRow]] = {
+    db.run {
+      (for (b <- Bosses) yield {
+        b
+      }).result
+    }
+  }
+  
+  def fetchItems(db: Database)(implicit ec: ExecutionContext): Future[Seq[ItemsRow]] = {
+    db.run {
+      (for (i <- Items) yield {
+        i
+      }).result
+    }
+  }
+  
+  def fetchWeapons(db: Database)(implicit ec: ExecutionContext): Future[Seq[WeaponsRow]] = {
+    db.run {
+      (for (w <- Weapons) yield {
+        w
+      }).result
+    }
+  }
+  
+  def fetchPlayers(db: Database)(implicit ec: ExecutionContext): Future[Seq[PlayerclassRow]] = {
+    db.run {
+      (for (p <- Playerclass) yield {
+        p
+      }).result
+    }
+  }
+  
+  def fetchUsers(db: Database)(implicit ec: ExecutionContext): Future[Seq[UserRow]] = {
+    db.run {
+      (for (u <- User) yield {
+        u
+      }).result
+    }
+  }
+}
