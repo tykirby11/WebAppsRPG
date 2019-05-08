371c851fb37e49c29e6f7111b61f5b74fe1e9470
2015-08-26 <agieselm@laurel.cs.trinity.edu>
2019-05-07 20:49:28 -0500

    finished map UI

--- client/src/main/scala/edu/trinity/webapps/Game2.scala
+++ client/src/main/scala/edu/trinity/webapps/Game2.scala
@@ -6,15 +6,58 @@ import org.scalajs.dom.raw._
 import org.scalajs.dom.raw.HTMLImageElement
 import scala.scalajs.js.timers._
 
-object CanvasDrawing {
+object CanvasMapDrawing {
 
   private val canvas = dom.document.getElementById("gameCanvas").asInstanceOf[dom.raw.HTMLCanvasElement]
   private val context = canvas.getContext("2d")
 
-
-  def drawArena(): Unit = {
+  //handling user input
+    dom.document.onmousedown = (e: dom.MouseEvent) => {
+    val coords = (e.clientX, e.clientY)
+      
+    println(s"x: ${coords._1}, y: ${coords._2}")
+      
+    //if left node is pressed, trigger event
+    if((e.clientX > 289 && e.clientX < 463) && (e.clientY > 380 && e.clientY < 454))
+      {
+      println("clicked left node, this should do something!")
+      println("Enemy Encounter triggered")
+      CanvasDrawing.drawArena()
+      }
+    //if right node is pressed, trigger event 
+    else if ((e.clientX > 589 && e.clientX < 764) && (e.clientY > 380 && e.clientY < 453))
+      {
+      println("clicked right node, this should do something!")
+      }
+    }  
+  
+  def drawMap(): Unit = {
+    
+    //background image for map view
     val mapBackground = dom.document.getElementById("mapBackground")
-    println(mapBackground) 
     context.drawImage(mapBackground, 0, 0, canvas.width, 600)
+    
+    //left node
+    context.rect(175, 240, 175, 75)
+    context.stroke()
+    context.lineWidth = "3"
+    context.fillStyle = "white"
+    context.font = "20px Arial"
+    context.fillText("Go Left For:", 180, 271)
+    context.fillText("Enemy Encounter", 180, 300);
+    
+    //right node
+    context.rect(475, 240, 175, 75)
+    context.stroke()
+    context.fillStyle = "white"
+    context.font = "20px Arial"
+    context.fillText("Go Right For:", 480, 271)
+    context.fillText("Mystery Event", 480, 300);
+    
+    //user score
+    context.fillText("Nodes Visited: 69", 620, 30)
+    
+    //gold
+    context.fillText("Gold: 100", 30, 30)
   }
 }
--- client/src/main/scala/edu/trinity/webapps/Main.scala
+++ client/src/main/scala/edu/trinity/webapps/Main.scala
@@ -7,7 +7,7 @@ object ScalaJSExample {
 
   def main(args: Array[String]): Unit = {
     dom.window.onload = (e: dom.raw.Event) => {
-      if(dom.document.getElementById("gameCanvas") != null) CanvasDrawing.drawArena()
+      if(dom.document.getElementById("gameCanvas") != null) CanvasMapDrawing.drawMap()//CanvasDrawing.drawArena()
     }
   }
   
