2556f1a4c9bbb2f3b40c1cb98b59e143ae6d4ea3
2015-08-26 <jmarti16@trinity.edu>
2019-05-08 19:11:35 -0500

    added css for mainmenu

--- client/src/main/scala/edu/trinity/webapps/Game.scala
+++ client/src/main/scala/edu/trinity/webapps/Game.scala
@@ -273,8 +273,8 @@ object CanvasDrawing {
       val tableHeaderBut = dom.document.createElement("th").asInstanceOf[HTMLElement];
       tableHeaderBut.innerHTML = "<button>Use</button>";
       tableHeaderBut.id = "useItem";
-      tableHeaderBut.onclick = { (e:Event) => deleteInvItem();}
       val tableRow = dom.document.createElement("tr").asInstanceOf[HTMLElement];
+      tableHeaderBut.onclick = { (e:Event) => tableHeaderBut.parentNode.parentNode.removeChild(tableRow);}
       
       conTable.appendChild(tableRow);
       tableRow.appendChild(tableHeaderItem);
